9f4bea7d8222600c65d44d08dbb228487c713678
Moritz Schönherr <moritz.schoenherr@gmail.com>
2018-03-13 15:38:43 +0100

    initial commit

---
+++ evenless.asd
@@ -0,0 +1,10 @@
+;;;; evenless.asd
+
+(asdf:defsystem #:evenless
+  :description "Describe evenless here"
+  :author "Your Name <your.name@example.com>"
+  :license  "Specify license here"
+  :version "0.0.1"
+  :serial t
+  :components ((:file "package")
+               (:file "evenless")))
---
+++ evenless.lisp
@@ -0,0 +1,38 @@
+;;;; evenless.lisp
+
+(in-package #:evenless)
+
+(defmacro define-nm-command (command (result-var) &body body)
+  "Given a symbol command that corresponds to a notmuch command,
+define a function that binds the output of
+notmuch command --format=sexp (search-string) to result-var and
+executes the body." ; possibly allow definition of functions of arity 0
+  (let ((command-name (string-downcase (symbol-name command)))
+	(full-name (intern (with-output-to-string (s)
+			     (princ 'nm- s)
+			     (princ command s)))))
+    `(defun ,full-name (search-string)
+       (let ((,result-var (read-from-string
+			  (with-output-to-string (out-s)
+			    (uiop:run-program
+			     (concatenate 'string "notmuch " ,command-name " --format=sexp " search-string)
+			     :output out-s)))))
+	 ,@body))))
+
+(defun pluck-a-pair (some-list)
+  "Plucks car and cadr of some-list and returns a (car .cadr)."
+  (cons (car some-list) (cadr some-list)))
+
+(defun list-to-alist (even-list)
+  "Takes a list with an even number of args and returns an alist."
+  (labels ((pluck-pairs (acc some-list)
+	     (if (<= (length some-list) 1)
+		 acc
+		 (pluck-pairs (cons (pluck-a-pair some-list) acc) (cddr some-list)))))
+    (reverse (pluck-pairs () even-list))))
+
+(define-nm-command search (result) ; to be written
+  (princ result))
+
+(define-nm-command show (result) ; to be written
+  (princ result))
